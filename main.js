49cc2c4958aee7c6e10cc4e1ab35ac0cb0897aab
enamul-007 <anamulkhan523@gmail.com>
2025-03-06 10:54:45 +0600

    Initial commit with project files

---
+++ main.js
@@ -0,0 +1,6 @@
+const budget_tracker = "personal budget tracker app < "
+const userName = "Enamul houque"
+const income = 30000
+const expenses=26000
+console.log(budget_tracker,userName,income,expenses);
+
