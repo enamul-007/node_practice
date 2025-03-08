1147624803fae93e5ed5400378da5b0ca6b8b58a
enamul-007 <anamulkhan523@gmail.com>
2025-03-08 16:53:27 +0600

    concatenate text add

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const budget_tracker = "personal budget tracker app < "
-const userName = "Enamul houque"
-const income = 30000
-const expenses=26000
-console.log(budget_tracker,userName,income,expenses);
+// const budget_tracker = "personal budget tracker app < "
+// const userName = "Enamul houque"
+// const income = 30000
+// const expenses=26000
+// console.log(budget_tracker,userName,income,expenses);
+
+
+
 
