3f9f53f3c8488727681bc1d80531a7718c18da53
enamul-007 <anamulkhan523@gmail.com>
2025-03-11 13:16:35 +0600

    new task add

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
 // const expenses=26000
 // console.log(budget_tracker,userName,income,expenses);
 
+const base = parseFloat(prompt("Enter Base : "));
+const base2 = parseFloat(prompt("Enter Base : "));
+const height = parseFloat(prompt("Enter Height : "));
 
-
-
+const area = (base + base2 ) *height / 2;
+console.log(area);
