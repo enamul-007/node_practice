1b8bedb377fa5044256e395698a0813b3939f3c4
enamul-007 <anamulkhan523@gmail.com>
2025-03-17 21:22:03 +0600

    new task add

--- main.js
+++ main.js
@@ -4,9 +4,21 @@
 // const expenses=26000
 // console.log(budget_tracker,userName,income,expenses);
 
-const base = parseFloat(prompt("Enter Base : "));
-const base2 = parseFloat(prompt("Enter Base : "));
-const height = parseFloat(prompt("Enter Height : "));
+// const base = parseFloat(prompt("Enter Base : "));
+// const base2 = parseFloat(prompt("Enter Base : "));
+// const height = parseFloat(prompt("Enter Height : "));
 
-const area = (base + base2 ) *height / 2;
-console.log(area);
+// const area = ((base + base2) * height) / 2;
+
+let number1 = prompt("Enter first number :");
+let number2 = prompt("Enter second number :");
+
+number1 = parseInt(number1, 10);
+number2 = parseInt(number2, 10);
+
+let sum, sub;
+sum = number1 + number2;
+sub = number1 - number2;
+document.write("Addition  = " + sum +  "<br/>");
+
+document.write("substraction is = " + sub);
