4742b547688fe1554edd059e531b69eea8eaad64
enamul-007 <anamulkhan523@gmail.com>
2025-04-19 13:42:09 +0600

    first commit

--- main.js
+++ main.js
@@ -1,38 +1,68 @@
-// const budget_tracker = "personal budget tracker app < "
-// const userName = "Enamul houque"
-// const income = 30000
-// const expenses=26000
-// console.log(budget_tracker,userName,income,expenses);
+//  personal budget tracker
+//  user : enamul hoque
+//  total income : 5000 $
+//  total expense : 2300
+//  tax deducated (10%)
+//  net income after tax
+//  remaining balance
+//  savings
 
-// const base = parseFloat(prompt("Enter Base : "));
-// const base2 = parseFloat(prompt("Enter Base : "));
-// const height = parseFloat(prompt("Enter Height : "));
+const userName = "Enamul hoque ";
+const income = 5000;
 
-// const area = ((base + base2) * height) / 2;
+// multipale expense
+const rent = 1200;
+const grocoris = 600;
+const transport = 200;
+const entertainmnet = 300;
+
+// total expences
+
+var totalExpanse = rent + grocoris + transport + entertainmnet;
+
+// tax deduction (10% of income )
+
+const tax = income * 0.1;
+
+// net income after the tax
+
+const netIncome = income - tax;
+
+// total remaining balance
+
+const balance = income - totalExpanse;
+
+// savings (20% of rimaining balance )
+
+const savings = balance * 0.2;
+
+console.log("personal budget tracker app");
+console.log("user name : " + userName);
+console.log("Total income $ " + income);
+console.log("Total Expense $ " + totalExpanse);
+console.log("Tax Deducated $ " + tax);
+console.log("Net income $" + netIncome);
+console.log("remaining balance $" + balance);
+console.log("Savings (20% of balance) $" + savings);
+
+var number1 = 20;
+var number2 = 30;
+var number3 = 40;
+
+console.log(number1 < number2);
+console.log((number1 = number2));
+console.log(number1 == number2);
+
+var number = 5;
+
+if(number % 2 == 0);
+console.log("Evenz");
+
+if( number % 2 != 0);
+console.log("Odd");
 
 
-// ============= Task 3 =============//
 
-let number1 = prompt("Enter first number :");
-let number2 = prompt("Enter second number :");
 
-number1 = parseInt(number1, 10);
-number2 = parseInt(number2, 10);
 
-let sum, sub, mul, division, modulus;
-sum = number1 + number2;
-sub = number1 - number2;
-mul = number1 * number2;
-division = number1 / number2;
-modulus = number1 % number2;
-
-document.write("Addition  = " + sum + "<br/>");
-
-document.write("substraction is = " + sub + "<br/>");
-
-document.write("Multiplication  is = " + mul + "<br/>");
-
-document.write("Division is = " + division + "<br/>");
-
-document.write("Modulus is = " + modulus + "<br/>");
 
