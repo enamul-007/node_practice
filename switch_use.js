fa020102bd9ce703c91ab10131d2acdbe24e4ec8
enamul-007 <anamulkhan523@gmail.com>
2025-04-21 13:07:30 +0600

    new task part add

---
+++ switch_use.js
@@ -0,0 +1,40 @@
+// digit spaling
+// 0 - zero ........ 9 - nine
+// switch , case , break , default
+var digit = prompt("Enter your digit :");
+
+switch (digit) {
+  case "0":
+    console.log("zero");
+    break;
+  case "1":
+    console.log("one");
+    break;
+  case "2":
+    console.log("tow");
+    break;
+  case "3":
+    console.log("three");
+    break;
+  case "4":
+    console.log("four");
+    break;
+  case "5":
+    console.log("five");
+    break;
+  case "6":
+    console.log("six");
+    break;
+  case "7":
+    console.log("seven");
+    break;
+  case "8 ":
+    console.log("eight");
+    break;
+  case "9":
+    console.log("nine");
+    break;
+
+  default:
+    console.log("not valid digit");
+}
