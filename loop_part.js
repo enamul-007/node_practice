470b83cc4c2f602ab62a2722e7b8e84cf16e3e68
enamul-007 <anamulkhan523@gmail.com>
2025-04-22 11:44:22 +0600

    loop part add

---
+++ loop_part.js
@@ -0,0 +1,8 @@
+var m = parseInt(prompt("enter your number"))
+var n = parseInt(prompt("enter your last number"))
+
+var sum = 0 
+for(var x = m ; x <= n ; x= x+1 ){
+   sum = sum + x
+}
+console.log(sum)
--- marks_main.js
+++ marks_main.js
@@ -1,19 +1,19 @@
 // student marks sheet
 
-var marks = prompt("Enter your marks :");
+// var marks = prompt("Enter your marks :");
 
-if (marks > 100 || marks < 0)
-     console.log("invalid marks");
-else if (marks > 79) 
-    console.log("A+");
-else if (marks > 69) 
-    console.log("A");
-else if (marks > 59)
-     console.log("A-");
-else if (marks > 49)
-     console.log("B");
-else if (marks > 39) 
-    console.log("C");
-else if (marks > 32) 
-    console.log("D");
-else console.log("Fail");
+// if (marks > 100 || marks < 0)
+//      console.log("invalid marks");
+// else if (marks > 79) 
+//     console.log("A+");
+// else if (marks > 69) 
+//     console.log("A");
+// else if (marks > 59)
+//      console.log("A-");
+// else if (marks > 49)
+//      console.log("B");
+// else if (marks > 39) 
+//     console.log("C");
+// else if (marks > 32) 
+//     console.log("D");
+// else console.log("Fail");
