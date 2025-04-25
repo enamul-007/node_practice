1bb3bdbcb26e8c5b4bfdc924913e4e080e7e9318
enamul-007 <anamulkhan523@gmail.com>
2025-04-25 23:42:15 +0600

    loop part add

--- loop_part.js
+++ loop_part.js
@@ -1,8 +1,15 @@
-var m = parseInt(prompt("enter your number"))
-var n = parseInt(prompt("enter your last number"))
+// var m = parseInt(prompt("enter your number"))
+// var n = parseInt(prompt("enter your last number"))
 
-var sum = 0 
-for(var x = m ; x <= n ; x= x+1 ){
-   sum = sum + x
+// var sum = 0 
+// for(var x = m ; x <= n ; x= x+1 ){
+//    sum = sum + x
+// }
+// console.log(sum)
+
+for(var x = 1; x <=5 ; x++){
+    var number1 = parseInt(prompt('enter your number'))
+    var number2 = parseInt(prompt('enter your second number'))
+    var sum = number1+number2
+    console.log('result : '+sum)
 }
-console.log(sum)
