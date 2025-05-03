52ade26e79bf856d437fce312ea6c9c1daffbf7a
enamul-007 <anamulkhan523@gmail.com>
2025-05-03 23:56:15 +0600

    function add

--- Task_6.js
+++ Task_6.js
@@ -1,54 +1,54 @@
-const userName = prompt("Enter your name :");
-const income = parseFloat(prompt("Enter your income :"));
-const numberOfExpense = parseInt(prompt("How many expense do you have? :"));
-if (
-  isNaN(income) ||
-  isNaN(numberOfExpense) ||
-  income <= 0 ||
-  numberOfExpense < 0
-) {
-  console.log("invalid inputs please input valid numbers ");
-} else {
-  let totalExpense = 0;
+// const userName = prompt("Enter your name :");
+// const income = parseFloat(prompt("Enter your income :"));
+// const numberOfExpense = parseInt(prompt("How many expense do you have? :"));
+// if (
+//   isNaN(income) ||
+//   isNaN(numberOfExpense) ||
+//   income <= 0 ||
+//   numberOfExpense < 0
+// ) {
+//   console.log("invalid inputs please input valid numbers ");
+// } else {
+//   let totalExpense = 0;
 
-  for (let i = 1; i <= numberOfExpense; i++) {
-    let expense = parseFloat(prompt(`Enter your expense ${i} :`));
-    if (isNaN(expense) || expense < 0) {
-      console.log(`invalid expense ${i} please input valid expense `);
-      expense = 0;
-    }
-    totalExpense += expense;
-  }
-  const tax = income * 0.1;
+//   for (let i = 1; i <= numberOfExpense; i++) {
+//     let expense = parseFloat(prompt(`Enter your expense ${i} :`));
+//     if (isNaN(expense) || expense < 0) {
+//       console.log(`invalid expense ${i} please input valid expense `);
+//       expense = 0;
+//     }
+//     totalExpense += expense;
+//   }
+//   const tax = income * 0.1;
 
-  // net income after the tax
+//   // net income after the tax
 
-  const netIncome = income - tax;
+//   const netIncome = income - tax;
 
-  // total remaining balance
+//   // total remaining balance
 
-  const balance = income - totalExpense;
+//   const balance = income - totalExpense;
 
-  // savings (20% of rimaining balance )
+//   // savings (20% of rimaining balance )
 
-  const savings = balance * 0.2;
+//   const savings = balance * 0.2;
 
-  var finalStatus = "";
+//   var finalStatus = "";
 
-  if (savings >= 1000) {
-    finalStatus = "Exccilent! you are saving very well!";
-  } else if (savings >= 500) {
-    finalStatus = "Good! you have a decent saivings ammount";
-  } else if (savings >= 100) {
-    finalStatus = "Needs improvement . considor redusing expense";
-  } else finalStatus = "critical you ar saving are too low";
-  console.log("personal budget tracker app");
-  console.log("user name : " + userName);
-  console.log("Total income $ " + income);
-  console.log("Total Expense $ " + totalExpense);
-  console.log("Tax Deducated $ " + tax);
-  console.log("Net income $" + netIncome);
-  console.log("remaining balance $" + balance);
-  console.log("Savings (20% of balance) $" + savings);
-  console.log(finalStatus);
-}
+//   if (savings >= 1000) {
+//     finalStatus = "Exccilent! you are saving very well!";
+//   } else if (savings >= 500) {
+//     finalStatus = "Good! you have a decent saivings ammount";
+//   } else if (savings >= 100) {
+//     finalStatus = "Needs improvement . considor redusing expense";
+//   } else finalStatus = "critical you ar saving are too low";
+//   console.log("personal budget tracker app");
+//   console.log("user name : " + userName);
+//   console.log("Total income $ " + income);
+//   console.log("Total Expense $ " + totalExpense);
+//   console.log("Tax Deducated $ " + tax);
+//   console.log("Net income $" + netIncome);
+//   console.log("remaining balance $" + balance);
+//   console.log("Savings (20% of balance) $" + savings);
+//   console.log(finalStatus);
+// }
---
+++ function.js
@@ -0,0 +1,7 @@
+function addition(number1, number2) {
+  var result = number1 * number2;
+
+  console.log("sub = " + result);
+}
+
+addition(6, 9);
